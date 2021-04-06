4f89b8fbf941165ef216fd0f2df5058b1282c00c
lars <lars.andren@defa.com>
2021-04-06 11:33:50 +0200

    Initial commit

---
+++ src/test/java/acds/com/oauthhackx/OauthHackxApplicationTests.java
@@ -0,0 +1,13 @@
+package acds.com.oauthhackx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthHackxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
